81a29fe87d6738ef30650fed754e2e4f73bcf5c8
Vedant Roy <vedantroy42@gmail.com>
2022-11-07 19:11:55 +0530

    Separate Filename creation
    look into replacement of Gxss as it is giving errors on installation and runtime

--- runfile.sh
+++ runfile.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 url=$1;
-mkdir $url;
-cd $url;
-gau $url --subs --blacklist pdf,css,jpeg,gif,svg,png,jpg,ttf,woff,js --fc 404,302 --fp | Gxss | tee $url.txt;
+filename=$2;
+mkdir $filename;
+cd $filename;
+gau $url --subs --blacklist pdf,css,jpeg,gif,svg,png,jpg,ttf,woff,js --fc 404,302 --fp | tee $url.txt;
+cat $url.txt | Gxss | tee output.txt;
 dalfox file $url.txt;
