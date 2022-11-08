71768ee4c4ebd6fd45d96fad24c26eaa3f723119
Vedant Roy <vedantroy42@gmail.com>
2022-11-08 10:43:07 +0530

    Changes in Gxss

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir output;
 go install github.com/lc/gau/v2/cmd/gau@latest;
 go install github.com/KathanP19/Gxss@latest;
 go install github.com/hahwul/dalfox/v2@latest;
--- runfile.sh
+++ runfile.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 url=$1;
 filename=$2;
+cd output;
 mkdir $filename;
 cd $filename;
-gau $url --subs --blacklist pdf,css,jpeg,gif,svg,png,jpg,ttf,woff,js --fc 404,302 --fp | tee $url.txt;
-cat $url.txt | Gxss | tee output.txt;
-dalfox file $url.txt;
+gau $url --subs --blacklist pdf,css,jpeg,gif,svg,png,jpg,ttf,woff,js,exe --fc 404,302 --fp | tee $url.txt;
+cat $url.txt | Gxss -c 100 -p Reflected | tee Reflected.txt;
+dalfox file Reflected.txt;
