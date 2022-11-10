2effee19813441d9f514c41fcd97ea946a465dae
Vedant Roy <vedantroy42@gmail.com>
2022-11-10 20:45:45 +0530

    Update 3
    Added katana and GAU both for better results

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ mkdir output;
 go install github.com/lc/gau/v2/cmd/gau@latest;
 go install github.com/KathanP19/Gxss@latest;
 go install github.com/hahwul/dalfox/v2@latest;
+go install github.com/tomnomnom/qsreplace@latest;
+go install github.com/projectdiscovery/katana/cmd/katana@latest;
+go install -v github.com/tomnomnom/anew@latest;
--- runfile.sh
+++ runfile.sh
@@ -4,6 +4,15 @@ filename=$2;
 cd output;
 mkdir $filename;
 cd $filename;
-gau $url --subs --blacklist pdf,css,jpeg,gif,svg,png,jpg,ttf,woff,js,exe --fc 404,302 --fp | tee $url.txt;
-cat $url.txt | Gxss -c 100 -p Reflected | tee Reflected.txt;
+echo RUNNING GAU
+#Gau working
+gau $url --subs --blacklist pdf,css,jpeg,gif,svg,png,jpg,ttf,woff,js,exe --fc 404,302 --fp | tee gauoutput.txt;
+#Katana working
+katana -u $url -fs fqdn -jc -d 5 -kf -aff -o katanaoutput.txt;
+cat gauoutput.txt | anew katanaoutput.txt;
+cat katanaoutput.txt | qsreplace parameter | grep parameter | tee parameter.txt;
+echo RUNNING GXSS
+#Gxss Running- write an echo
+cat parameter.txt | Gxss -c 100 -p Reflected | tee Reflected.txt;
+#Dalfox Running- write an echo
 dalfox file Reflected.txt;
